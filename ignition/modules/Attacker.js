ebec6e5db103f021400309f78ddc5e5bd3a33ad3
akaMiWP <bangkokappdevelopment@gmail.com>
2024-05-13 19:04:49 +0700

    Updated deployed addresses

--- ignition/modules/Attacker.js
+++ ignition/modules/Attacker.js
@@ -2,8 +2,8 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("DeployAttacker", (m) => {
   const attacker = m.contract("Attacker", [
-    "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-    m.getAccount(1),
+    "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+    m.getAccount(0),
   ]);
   return { attacker };
 });
